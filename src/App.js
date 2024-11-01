14a30b23b266d8401e64ce96b6aed9eb78d7de0d
Ruslan Hayduk <rgayduk86e@gmail.com>
2024-11-01 20:41:34 +0200

    added decrement button

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
 import { useState } from "react";
-import {sum} from "./helpers/sum/sum";
+import { sum } from "./helpers/sum/sum";
+import {minus} from "./helpers/minus/minus";
 
 function App() {
     const [state, setState] = useState(0)
 
     const increment = () => setState(prev => sum(prev, 1));
+    const decrement = () => setState(prev => minus(prev, 1));
+    const get_color = () => {
+        console.log(state);
+        
+        return state >= 0 ? 'blue' : 'red'
+    }
 
     return (
         <div className="App">
-            <h1 style={{fontSize: 70, color: 'blue'}}>value = {state}</h1>
+            <h1 style={{ fontSize: 70, color: {get_color} }}>value = {state}</h1>
             <button
                 style={{background: 'black', color: "white", padding: 15}}
                 onClick={increment}
             >
-                incremernt
+                increment
             </button>
+
+            <button
+                style={{background: 'black', color: "red", padding: 15}}
+                onClick={decrement}
+            >
+                decrement
+            </button>
+
         </div>
     );
 }
